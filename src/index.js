858d7b1fc3d6521e637bb2df5e076f027a3e2de4
chapi <chapi@chapis-MacBook-Pro.local>
2023-06-20 13:41:57 -0400

    update

--- src/index.js
+++ src/index.js
@@ -16,78 +16,79 @@ try {
    if (scanType === 'zap') {
      // Parse the json file from Zap scan 
      function parseZapOutput(jsonData) {
-     let vulnerabilities = [];
-     if (jsonData) {
-       try {
-         const data = JSON.parse(jsonData);
-         for (const site of data.site) {
-           for (const alert of site.alerts) {
-             let instanceDescription = '\nInstances:\n';
-             for (const instance of alert.instances) {
-               const { uri, method, param, attack, evidence, otherinfo } = instance;
-               instanceDescription += `URI: ${uri}\nMethod: ${method}\nParam: ${param}\nAttack: ${attack}\nEvidence: ${evidence}\nOtherInfo: ${otherinfo}\n\n`;
-             }
-               if (alert.riskcode >= parseInt(core.getInput('zap-risk-code'))) {
-                   vulnerabilities.push({
-                     name: alert.name.concat(': ', data.site[0]['@host']).replace(/-/g, ''),
-                     solution: alert.solution.replace(/<\/?p>/g, ''),
-                     desc: alert.desc.concat('\n\nSolution:\n', alert.solution).replace(/<\/?p>/g, ''),
-                     instanceDesc: instanceDescription
-                   });
+ 
+       let vulnerabilities = [];
+       if (jsonData) {
+         try {
+           const data = JSON.parse(jsonData);
+           for (const site of data.site) {
+             for (const alert of site.alerts) {
+               let instanceDescription = '\nInstances:\n';
+               for (const instance of alert.instances) {
+                 const { uri, method, param, attack, evidence, otherinfo } = instance;
+                 instanceDescription += `URI: ${uri}\nMethod: ${method}\nParam: ${param}\nAttack: ${attack}\nEvidence: ${evidence}\nOtherInfo: ${otherinfo}\n\n`;
                }
+               if (alert.riskcode >= parseInt(core.getInput('zap-risk-code'))) {
+                 vulnerabilities.push({
+                   name: alert.name.concat(': ', data.site[0]['@host']).replace(/-/g, ''),
+                   solution: alert.solution.replace(/<\/?p>/g, ''),
+                   desc: alert.desc.concat('\n\nSolution:\n', alert.solution).replace(/<\/?p>/g, ''),
+                   instanceDesc: instanceDescription
+                 });
+               }
+             }
            }
+         } catch (error) {
+           console.error('Error parsing Zap output:', error);
          }
-       } catch (error) {
-         console.error('Error parsing Zap output:', error);
        }
-     }
-     console.log(vulnerabilities);
-     return vulnerabilities;
-     }
+       console.log(vulnerabilities);
+       return vulnerabilities;
+      }
      
      async function createJiraTicket(vulnerability) {
     
-     let jqlQuery = `project = "${core.getInput('jira-project-key')}" AND summary ~ "MCR - SNYK ${vulnerability.name}" AND created >= startOfDay("-60d") AND status NOT IN ("Closed")`;
-     let searchResult = await jira.searchJira(jqlQuery);
-  
-     if (!searchResult.issues || searchResult.issues.length === 0) {
-       const customFieldKeyValue = JSON.parse(core.getInput('jira-custom-field-key-value'));
-       const customJiraFields = {
-         ...customFieldKeyValue,
-       };
-       const issue = {
-         fields: {
-           project: {
-             key: core.getInput('jira-project-key'),
+       let jqlQuery = `project = "${core.getInput('jira-project-key')}" AND summary ~ "MCR - SNYK ${vulnerability.name}" AND created >= startOfDay("-60d") AND status NOT IN ("Closed")`;
+       let searchResult = await jira.searchJira(jqlQuery);
+    
+       if (!searchResult.issues || searchResult.issues.length === 0) {
+         const customFieldKeyValue = JSON.parse(core.getInput('jira-custom-field-key-value'));
+         const customJiraFields = {
+           ...customFieldKeyValue,
+         };
+         const issue = {
+           fields: {
+             project: {
+               key: core.getInput('jira-project-key'),
+             },
+             summary: core.getInput('jira-title-prefix').concat(' ', vulnerability.name),
+             description: vulnerability.desc.concat('\n', vulnerability.instanceDesc),
+             issuetype: {
+               name: core.getInput('jira-issue-type'),
+             },
+             labels: core.getInput('jira-labels').split(','),
+             ...customJiraFields,
+             
            },
-           summary: core.getInput('jira-title-prefix').concat(' ', vulnerability.name),
-           description: vulnerability.desc.concat('\n', vulnerability.instanceDesc),
-           issuetype: {
-             name: core.getInput('jira-issue-type'),
-           },
-           labels: core.getInput('jira-labels').split(','),
-           ...customJiraFields,
-           
-         },
-       };
+         };
      
-       const issueResponse = await jira.addNewIssue(issue);
-       console.log(`Jira ticket created for vulnerability: ${vulnerability.name}`);
+         const issueResponse = await jira.addNewIssue(issue);
+         console.log(`Jira ticket created for vulnerability: ${vulnerability.name}`);
+         
+         process.env.SCAN_OUTPUT_FILE_PATH = core.getInput('scan-output-path');
+         const scanOutputFilePath = process.env.SCAN_OUTPUT_FILE_PATH
        
-       process.env.SCAN_OUTPUT_FILE_PATH = core.getInput('scan-output-path');
-       const scanOutputFilePath = process.env.SCAN_OUTPUT_FILE_PATH
-     
-       try {
-         // Use the addAttachmentOnIssue method from the Jira library
-         await jira.addAttachmentOnIssue(issueResponse.key, fs.createReadStream(scanOutputFilePath));
-         console.log(`Jira ticket ${issueResponse.key} created successfully.`);
-       } catch (error) {
-         console.error(`Error adding attachment to Jira ticket ${issueResponse.key}:`, error);
+         try {
+           // Use the addAttachmentOnIssue method from the Jira library
+           await jira.addAttachmentOnIssue(issueResponse.key, fs.createReadStream(scanOutputFilePath));
+           console.log(`Jira ticket ${issueResponse.key} created successfully.`);
+         } catch (error) {
+           console.error(`Error adding attachment to Jira ticket ${issueResponse.key}:`, error);
+         }
+         return issueResponse;
+       } else {
+          console.log(`Active Jira ticket already exists for vulnerability: ${vulnerability.name}`);
        }
-       return issueResponse;
-     } else {
-       console.log(`Active Jira ticket already exists for vulnerability: ${vulnerability.name}`);
-     }
      }
      
      (async () => {
