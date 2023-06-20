e63d7f74764d42784ae9f9d855f5be5d51821b65
chapi <chapi@chapis-MacBook-Pro.local>
2023-06-20 17:41:12 -0400

    fixed zap duplicate

--- dist/index.js
+++ dist/index.js
@@ -58159,8 +58159,7 @@ try {
     //  let jqlQuery = `project = "${core.getInput('jira-project-key')}" AND summary ~ "${core.getInput('jira-title-prefix').concat(' ', vulnerability.name)}" AND created >= startOfDay("-60d") AND status NOT IN ("Closed")`;
     //  let searchResult = await jira.searchJira(jqlQuery);
 
-     let jqlQuery = `project = "${core.getInput('jira-project-key')}" AND summary ~ "${core.getInput('jira-title-prefix').concat(' ', vulnerability.name)}" AND created >= startOfDay("-60d") AND status NOT IN ("Closed")`;
-     console.log('JQL Query:', jqlQuery); // Print the JQL query
+     let jqlQuery = `project = "${core.getInput('jira-project-key')}" AND summary ~ "${vulnerability.name}" AND created >= startOfDay("-60d") AND status NOT IN ("Closed")`;
      let searchResult = await jira.searchJira(jqlQuery);
       
      if (!searchResult.issues || searchResult.issues.length === 0) {
@@ -58264,7 +58263,6 @@ try {
        // JQL query with relative date math, status conditions. 
        const title = vulnerability.title.replaceAll("\"", "\\\"");
        let jqlQuery = `project = "${core.getInput('jira-project-key')}" AND summary ~ "MCR - SNYK ${vulnerability.title}" AND created >= startOfDay("-60d") AND status NOT IN ("Closed", "Cancelled")`;
-     
        let searchResult = await jira.searchJira(jqlQuery);
      
        if (!searchResult.issues || searchResult.issues.length === 0) {
