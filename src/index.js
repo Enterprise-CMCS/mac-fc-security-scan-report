85785fa7b6f907f395497d8fd4154db01327348f
araya <araya@arayas-MacBook-Pro.local>
2023-11-22 15:34:54 -0500

    update

--- src/index.js
+++ src/index.js
@@ -227,8 +227,6 @@ try {
         const title = vulnerability.title.replaceAll("\"", "\\\"");
         const jqlQuery = `project = "${core.getInput('jira-project-key')}" AND summary ~ "${vulnerability.title}" AND created >= startOfDay("-60d") AND status != "Canceled"`;
         const searchResponse = await jira.get('/rest/api/2/search', { params: { jql: jqlQuery } });
-        
-        console.log('Jira Search Response:', searchResponse.status, searchResponse.data);
 
         const searchResult = searchResponse.data; 
         if (searchResponse.status === 200) {
