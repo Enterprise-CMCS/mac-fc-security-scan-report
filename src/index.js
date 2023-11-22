100fe2671b1ea7f16bf44a9940257abb7e7a74b7
araya <araya@arayas-MacBook-Pro.local>
2023-11-22 12:57:39 -0500

    update

--- src/index.js
+++ src/index.js
@@ -295,6 +295,9 @@ try {
 
       for (const vulnerability of uniqueVulnerabilities) {
         try {
+          console.log(vulnerability);
+          console.log('******** Jira ticket payload: *********');
+          console.log(issue);
           console.log(`Creating Jira ticket for vulnerability: ${vulnerability.title}`);
           const resp = await createSnykJiraTicket(vulnerability);
           console.log(resp)
